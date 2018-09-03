d77d5a7e97f6e17f6ff3eb78ac0aa27d8f095f05
Jaime McCutcheon <jem64@le.ac.uk>
2018-09-03 15:55:02 +0100

    updated file for stats
    needs to be integrated with main scripts

--- temp.py
+++ temp.py
@@ -5,16 +5,26 @@ Created on Mon Mar  5 10:39:12 2018
 @author: jaimeHP
 """
 
-from subprocess import Popen, PIPE, run
+
+"""
+Code for running stats using R
+
+This requires R to be installed and an Rscript written. At the moment I am using
+the R package, EZ, which makes running mixed, between-within ANOVAs simple, and
+tests for sphericity etc as appropriate.
+
+EZ can be installed using the command install.packages('ez') in R. The package
+seems to work best in R3.4.4 or later.
+
+An R script is written to run the analysis and print the results. This script
+is then called by Rscript.exe via the subprocess module in Python.
+
+"""
+
+from subprocess import PIPE, run
 
 Rscriptpath = 'C:\\Program Files\\R\\R-3.5.1\\bin\\Rscript'
 Rprogpath = 'C:\\Users\\James Rig\\Documents\\GitHub\\PPP_analysis\\bw_fi_stats.R'
-
-#subprocess.call("C:\\Users\\James Rig\\Documents\\GitHub\\PPP_analysis\\test.Rexec")
-
-#pipe = subprocess.Popen([Rscriptpath, "--vanilla", Rprogpath], stdout=PIPE)
-#
-#text = pipe.communicate()[0]
 
 
 result = run([Rscriptpath, "--vanilla", Rprogpath], stdout=PIPE, stderr=PIPE, universal_newlines=True)
