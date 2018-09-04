179369641bd505d18f789be20f9fb2c66ce59c84
Jaime McCutcheon <jem64@le.ac.uk>
2018-09-04 12:45:13 +0100

    worked out way of selecting with multiindex

--- temp.py
+++ temp.py
@@ -5,27 +5,12 @@ Created on Mon Mar  5 10:39:12 2018
 @author: jaimeHP
 """
 
+xlfile = 'R:\\DA_and_Reward\\gc214\\PPP_combined\\PPP_body weight and food intake.xlsx'
 
-"""
-Code for running stats using R
+# Body weight data
+df = pd.read_excel(xlfile, sheet_name='PPP_bodyweight')
+df.set_index(['rat', 'diet'], inplace=True)
 
-This requires R to be installed and an Rscript written. At the moment I am using
-the R package, EZ, which makes running mixed, between-within ANOVAs simple, and
-tests for sphericity etc as appropriate.
+df.iloc[:, df.rows.get_level_values('diet') == 'NR']
 
-EZ can be installed using the command install.packages('ez') in R. The package
-seems to work best in R3.4.4 or later.
-
-An R script is written to run the analysis and print the results. This script
-is then called by Rscript.exe via the subprocess module in Python.
-
-"""
-
-from subprocess import PIPE, run
-
-Rscriptpath = 'C:\\Program Files\\R\\R-3.5.1\\bin\\Rscript'
-Rprogpath = 'C:\\Users\\James Rig\\Documents\\GitHub\\PPP_analysis\\bw_fi_stats.R'
-
-result = run([Rscriptpath, "--vanilla", Rprogpath], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-
-print(result.returncode, result.stderr, result.stdout)
+df.xs('PR', level=1)
