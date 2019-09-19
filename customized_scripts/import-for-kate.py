b305aaae368a7c01d7bba98a68508c018a20dad0
Jaime McCutcheon <mccutcheon.james@gmail.com>
2019-09-19 09:04:06 +0200

    Create import-for-kate.py

---
+++ customized_scripts/import-for-kate.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 15:41:31 2019
+
+@author: jmc010
+"""
+# Import modules
+import tdt
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+
+
+def correctforbaseline(blue, uv):
+    pt = len(blue)
+    X = np.fft.rfft(uv, pt)
+    Y = np.fft.rfft(blue, pt)
+    Ynet = Y-X
+
+    datafilt = np.fft.irfft(Ynet)
+
+    datafilt = sig.detrend(datafilt)
+
+    b, a = sig.butter(9, 0.012, 'low', analog=True)
+    datafilt = sig.filtfilt(b, a, datafilt)
+    
+    return datafilt
+
+
+# Set variables - hardcoded here but could be read in via metafile
+tdtfile='C:\\Github\\PPP_analysis\\data\\Eelke-171027-111329\\'
+SigBlue='Dv1B'
+SigUV='Dv2B'
+
+ttl_licksL='LL1_'
+ttl_licksR='RL1_'
+
+
+# Read streamed data in
+tmp = tdt.read_block(tdtfile, evtype=['streams'], store=[SigBlue])
+data_blue = getattr(tmp.streams, SigBlue)['data']
+fs = getattr(tmp.streams, SigBlue)['fs']
+
+tmp = tdt.read_block(tdtfile, evtype=['streams'], store=[SigUV])
+data_uv = getattr(tmp.streams, SigUV)['data']
+
+
+# Perform baseline correction
+data_filt = correctforbaseline(data_blue, data_uv)
+
+
+
+# Read in TTLs
+ttls = tdt.read_block(tdtfile, evtype=['epocs']).epocs
+
+lt = getattr(ttls, ttl_licksL)
+rt = getattr(ttls, ttl_licksR)
+
+
+
+# Plots data - x-axis of lower panel does not match upper two panels but you
+# get the idea
+f, ax = plt.subplots(nrows=3, figsize=(8,8))
+ax[0].plot(data_blue, color='blue')
+ax[0].plot(data_uv, color='pink')
+
+ax[1].plot(data_filt, color='green')
+
+ax[2].scatter(lt.onset, [1]*len(lt.onset))
+ax[2].scatter(rt.onset, [2]*len(rt.onset))
+ax[2].set_ylim([0, 3])
