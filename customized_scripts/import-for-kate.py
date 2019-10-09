962d03a947e8b1f668326fc2c026ea70b426b29d
Jaime McCutcheon <mccutcheon.james@gmail.com>
2019-10-09 08:33:17 +0200

    Experimented with tick store

--- customized_scripts/import-for-kate.py
+++ customized_scripts/import-for-kate.py
@@ -10,6 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
 
+import timeit
+
 
 def correctforbaseline(blue, uv):
     pt = len(blue)
@@ -29,43 +31,54 @@ def correctforbaseline(blue, uv):
 
 # Set variables - hardcoded here but could be read in via metafile
 tdtfile='C:\\Github\\PPP_analysis\\data\\Eelke-171027-111329\\'
+tdtfile='C:\\Github\\PPP_analysis\\data\\Giulia-190930-102422\\'
 SigBlue='Dv1B'
 SigUV='Dv2B'
 
 ttl_licksL='LL1_'
 ttl_licksR='RL1_'
 
+tic = timeit.default_timer()
+#tmp=tdt.read_block(tdtfile, nodata=True)
+tmp = tdt.read_block(tdtfile, t2=2, evtype=['streams'])
+toc = timeit.default_timer()
+
+print(toc-tic)
+print(tmp.streams)
+
 
 # Read streamed data in
 tmp = tdt.read_block(tdtfile, evtype=['streams'], store=[SigBlue])
-data_blue = getattr(tmp.streams, SigBlue)['data']
-fs = getattr(tmp.streams, SigBlue)['fs']
-
-tmp = tdt.read_block(tdtfile, evtype=['streams'], store=[SigUV])
-data_uv = getattr(tmp.streams, SigUV)['data']
-
-
-# Perform baseline correction
-data_filt = correctforbaseline(data_blue, data_uv)
-
-
-
-# Read in TTLs
+#data_blue = getattr(tmp.streams, SigBlue)['data']
+#fs = getattr(tmp.streams, SigBlue)['fs']
+#
+#tmp = tdt.read_block(tdtfile, evtype=['streams'], store=[SigUV])
+#data_uv = getattr(tmp.streams, SigUV)['data']
+#
+#
+## Perform baseline correction
+#data_filt = correctforbaseline(data_blue, data_uv)
+#
+#
+#
+## Read in TTLs
 ttls = tdt.read_block(tdtfile, evtype=['epocs']).epocs
-
-lt = getattr(ttls, ttl_licksL)
-rt = getattr(ttls, ttl_licksR)
-
-
-
-# Plots data - x-axis of lower panel does not match upper two panels but you
-# get the idea
-f, ax = plt.subplots(nrows=3, figsize=(8,8))
-ax[0].plot(data_blue, color='blue')
-ax[0].plot(data_uv, color='pink')
-
-ax[1].plot(data_filt, color='green')
-
-ax[2].scatter(lt.onset, [1]*len(lt.onset))
-ax[2].scatter(rt.onset, [2]*len(rt.onset))
-ax[2].set_ylim([0, 3])
+#
+#lt = getattr(ttls, ttl_licksL)
+#rt = getattr(ttls, ttl_licksR)
+#
+## Get ticks if not ticks saved
+sc = tdt.read_block(tdtfile, evtype=['scalars']).scalars
+#scalars.scalars.Pars.ts(1:2:end)
+#
+## Plots data - x-axis of lower panel does not match upper two panels but you
+## get the idea
+#f, ax = plt.subplots(nrows=3, figsize=(8,8))
+#ax[0].plot(data_blue, color='blue')
+#ax[0].plot(data_uv, color='pink')
+#
+#ax[1].plot(data_filt, color='green')
+#
+#ax[2].scatter(lt.onset, [1]*len(lt.onset))
+#ax[2].scatter(rt.onset, [2]*len(rt.onset))
+#ax[2].set_ylim([0, 3])
---
+++ customized_scripts/tick-stores.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 15:41:31 2019
+
+@author: jmc010
+"""
+# Import modules
+import tdt
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+
+import timeit
+
+
+def correctforbaseline(blue, uv):
+    pt = len(blue)
+    X = np.fft.rfft(uv, pt)
+    Y = np.fft.rfft(blue, pt)
+    Ynet = Y-X
+
+    datafilt = np.fft.irfft(Ynet)
+
+    datafilt = sig.detrend(datafilt)
+
+    b, a = sig.butter(9, 0.012, 'low', analog=True)
+    datafilt = sig.filtfilt(b, a, datafilt)
+    
+    return datafilt
+
+
+# Set variables - hardcoded here but could be read in via metafile
+tdtfile='C:\\Github\\PPP_analysis\\data\\Eelke-171027-111329\\'
+
+
+ttls = tdt.read_block(tdtfile, evtype=['epocs']).epocs
+sc = tdt.read_block(tdtfile, evtype=['scalars']).scalars
+
+ticks1=ttls.Tick.onset
+
+ticks2=sc.Pars.ts
+
+
+#tdtfile='C:\\Github\\PPP_analysis\\data\\Giulia-190930-102422\\'
+#
+#sc = tdt.read_block(tdtfile, evtype=['scalars']).scalars
+#
+#ticks2=sc.Pars.ts
+#
+#print(len(ticks1), len(ticks2))
+#
+##plt.plot(ticks1, ticks2[:2474])
+#
+#plt.plot(ticks2[:60])
+#
+#print([np.floor(x) for x in ticks2[:20]])
+#
+#
+#
+#
+SigBlue='Dv1B'
+SigUV='Dv2B'
+#
+#ttl_licksL='LL1_'
+#ttl_licksR='RL1_'
+#
+#tic = timeit.default_timer()
+##tmp=tdt.read_block(tdtfile, nodata=True)
+#tmp = tdt.read_block(tdtfile, t2=2, evtype=['streams'])
+#toc = timeit.default_timer()
+#
+#print(toc-tic)
+#print(tmp.streams)
+#
+#
+## Read streamed data in
+tmp = tdt.read_block(tdtfile, evtype=['streams'], store=[SigBlue])
+data_blue = getattr(tmp.streams, SigBlue)['data']
+fs = getattr(tmp.streams, SigBlue)['fs']
+
+time_blue = np.linspace(1, len(data_blue), len(data_blue)) / fs
+
+plt.plot(time_blue, data_blue)
+##
+##tmp = tdt.read_block(tdtfile, evtype=['streams'], store=[SigUV])
+##data_uv = getattr(tmp.streams, SigUV)['data']
+##
+##
+### Perform baseline correction
+##data_filt = correctforbaseline(data_blue, data_uv)
+##
+##
+##
+### Read in TTLs
+#ttls = tdt.read_block(tdtfile, evtype=['epocs']).epocs
+##
+##lt = getattr(ttls, ttl_licksL)
+##rt = getattr(ttls, ttl_licksR)
+##
+### Get ticks if not ticks saved
+#sc = tdt.read_block(tdtfile, evtype=['scalars']).scalars
+##scalars.scalars.Pars.ts(1:2:end)
+##
+### Plots data - x-axis of lower panel does not match upper two panels but you
+### get the idea
+##f, ax = plt.subplots(nrows=3, figsize=(8,8))
+##ax[0].plot(data_blue, color='blue')
+##ax[0].plot(data_uv, color='pink')
+##
+##ax[1].plot(data_filt, color='green')
+##
+##ax[2].scatter(lt.onset, [1]*len(lt.onset))
+##ax[2].scatter(rt.onset, [2]*len(rt.onset))
+##ax[2].set_ylim([0, 3])
