4c4f92b96cf3240bb7c78b5a82b801ee7e069f48
Jaime McCutcheon <mccutcheon.james@gmail.com>
2020-01-14 12:02:42 +0100

    Creation of process_rat.py

---
+++ notebooks/process_rat.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 11:52:45 2020
+
+@author: admin
+"""
+
+"""
+This script is based on the Jupyter Notebook, process_rat.ipynb, which should
+be used to test edits to it and associated functions. It uses helper functions
+located in several fx-for-... files
+"""
+
+import tdt
+import numpy as np
+import matplotlib.pyplot as plt
+
+import scipy.signal as sig
+#
+import fx4assembly
+import fx4behavior
+import fx4makingsnips
+
+#def process_rat(tank):
+    
